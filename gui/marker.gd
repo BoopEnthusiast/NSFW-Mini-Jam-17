cf73ec7dc9a4e5499b6c2ac9ed32c1c1dc5fffa5
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-12 18:49:27 +1200

    better minimap circles

--- gui/marker.gd
+++ gui/marker.gd
@@ -1,16 +1,2 @@
-@tool
 class_name Marker
-extends TextureRect
-
-
-@export var color: Color:
-	set(value):
-		color = value
-		if is_instance_valid(color_rect):
-			color_rect.color = value
-
-@onready var color_rect: ColorRect = $ColorRect
-
-
-func _ready() -> void:
-	color_rect.color = color
+extends ColorRect
