5b0e6268cca01eada8f763e448d66862228d8896
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-14 15:01:28 +1200

    speed emitter

--- core/player.gd
+++ core/player.gd
@@ -10,7 +10,8 @@ const BRAKE_FORCE = 100
 var reversing: bool = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var fmod_event_emitter_3d: FmodEventEmitter3D = $FmodEventEmitter3D
+@onready var engine_force_emitter: FmodEventEmitter3D = $EngineForce
+@onready var speed_emitter: FmodEventEmitter3D = $Speed
 
 var collected_people: Array[Person.Gender] = []
 
