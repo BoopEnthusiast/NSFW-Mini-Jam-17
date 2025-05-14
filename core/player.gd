18e869e70ef800f6cc78ee84380530cf4d812936
Jack Flynn <jackflynnaudio@gmail.com>
2025-05-13 19:11:56 -0700

    FMOD fuckery
    - moved sound assets and fmod save stuff into new folder
    - added a few new sound events

--- core/player.gd
+++ core/player.gd
@@ -49,10 +49,9 @@ func _physics_process(_delta: float) -> void:
 		rotation.z = 0
 	
 	# Fmod events
-	#fmod_event_emitter_3d.set_parameter("speed", linear_velocity.length())
+	##fmod_event_emitter_3d.set_parameter("speed", linear_velocity.length())
 	fmod_event_emitter_3d.set_parameter("engine_force", engine_force)
 	
-	#Fmod test event
 	
 
 
