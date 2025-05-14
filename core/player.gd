7fe3090b665e70a8ef1f109c758aca99eb08cea3
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-14 19:06:36 +1200

    get people to say hey

--- core/player.gd
+++ core/player.gd
@@ -64,3 +64,9 @@ func _physics_process(_delta: float) -> void:
 					Person.Gender.FEMALE:
 						fem_greeting.play()
 				body.queue_free()
+
+
+func _on_collect_area_body_entered(body: Node3D) -> void:
+	if body is Person:
+		if collected_people.size() < 2:
+			body.say_hey()
