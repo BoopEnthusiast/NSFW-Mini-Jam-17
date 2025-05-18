c0870a3187e24665d9537c3da8ce74ce254a4efe
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-18 20:46:13 +1200

    final game

--- art/human/fem_base_model.gd
+++ art/human/fem_base_model.gd
@@ -2,4 +2,16 @@ class_name FemBaseModel
 extends Node3D
 
 
+@export var which_mesh: int = 0
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var meshes = [$"Fem Rig/Skeleton3D/Fem 1", $"Fem Rig/Skeleton3D/Fem 1 Nude"]
+
+
+func _ready() -> void:
+	for i in range(meshes.size()):
+		if i == which_mesh:
+			meshes[i].visible = true
+		else:
+			meshes[i].visible = false
