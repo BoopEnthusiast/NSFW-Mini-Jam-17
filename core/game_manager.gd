a4f1e8cd4098a52240949582c2219bd19e8c2772
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-18 15:43:09 +1200

    background music changes over time

--- core/game_manager.gd
+++ core/game_manager.gd
@@ -4,11 +4,19 @@ extends Node
 
 @onready var main: Main = $Main
 @onready var main_menu: MainMenu = $WhenPausedLayer/MainMenu
+@onready var bgm: FmodEventEmitter3D = $FmodBankLoader/bgm
 
 
 func _ready() -> void:
 	Nodes.player.camera_animator.play("swing_to_behind")
 	get_tree().paused = true
+
+
+func _process(delta: float) -> void:
+	if Nodes.main.time_left <= 30.0:
+		bgm.set_parameter("bgmIntensity", "levelLate")
+	elif Nodes.main.time_left <= 70.0:
+		bgm.set_parameter("bgmIntensity", "levelEarly")
 
 
 func _on_main_menu_play_game() -> void:
