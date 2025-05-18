efe892bd2f0028e66235f5844516b233b57659cf
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-05-19 10:48:27 +1200

    fix meshes

--- art/human/fem_base_model.gd
+++ art/human/fem_base_model.gd
@@ -2,16 +2,25 @@ class_name FemBaseModel
 extends Node3D
 
 
-@export var which_mesh: int = 0
+@export var nude: bool = false
+
+var which_mesh: int = 0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var meshes = [$"Fem Rig/Skeleton3D/Fem 1", $"Fem Rig/Skeleton3D/Fem 1 Nude"]
+var meshes = [$"Fem Rig/Skeleton3D/Fem 1", $"Fem Rig/Skeleton3D/Fem 2", $"Fem Rig/Skeleton3D/Fem 3"]
+var nude_meshes = [$"Fem Rig/Skeleton3D/Fem 1 Nude", $"Fem Rig/Skeleton3D/Fem 2 Nude", $"Fem Rig/Skeleton3D/Fem 3 Nude"]
 
 
 func _ready() -> void:
-	for i in range(meshes.size()):
-		if i == which_mesh:
-			meshes[i].visible = true
-		else:
-			meshes[i].visible = false
+	which_mesh = randi_range(0, 2)
+	
+	for mesh in meshes:
+		mesh.visible = false
+	for mesh in nude_meshes:
+		mesh.visible = false
+	
+	if nude:
+		nude_meshes[which_mesh].visible = true
+	else:
+		meshes[which_mesh].visible = true
--- art/human/masc_base_model.gd
+++ art/human/masc_base_model.gd
@@ -2,16 +2,25 @@ class_name MascBaseModel
 extends Node3D
 
 
-@export var which_mesh: int = 0
+@export var nude: bool = false
+
+var which_mesh: int = 0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var meshes = [$"Male Rig/Skeleton3D/Masc 1", $"Male Rig/Skeleton3D/Masc 1 Nude"]
+var meshes = [$"Male Rig/Skeleton3D/Masc 1", $"Male Rig/Skeleton3D/Masc 2", $"Male Rig/Skeleton3D/Masc 3"]
+var nude_meshes = [$"Male Rig/Skeleton3D/Masc 1 Nude", $"Male Rig/Skeleton3D/Masc 2 Nude", $"Male Rig/Skeleton3D/Masc 3 Nude"]
 
 
 func _ready() -> void:
-	for i in range(meshes.size()):
-		if i == which_mesh:
-			meshes[i].visible = true
-		else:
-			meshes[i].visible = false
+	which_mesh = randi_range(0, 2)
+	
+	for mesh in meshes:
+		mesh.visible = false
+	for mesh in nude_meshes:
+		mesh.visible = false
+	
+	if nude:
+		nude_meshes[which_mesh].visible = true
+	else:
+		meshes[which_mesh].visible = true
